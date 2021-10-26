8ed3c03f2cabe79f5b14507ca4e9c01694b6dada
Matheus da Silva <ms25022003@gmail.com>
2021-10-26 10:19:31 -0300

    .

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
-import Header from './components/header/header';
+// import Header from './components/header/header';
 import Main from './components/main/main';
-import Footer from './components/footer/footer';
+// import Footer from './components/footer/footer';
 
 // import './App.css';
 function App() {
