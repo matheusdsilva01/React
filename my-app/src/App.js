6b26c9d2f455b0386d8bf68e1f3abf51d9da3f37
Matheus da Silva <ms25022003@gmail.com>
2021-10-26 09:59:14 -0300

    alterações feitas para realização de atividade na mesa de trabalho, dia 26/10/2021

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,13 @@ import Header from './components/header/header';
 import Main from './components/main/main';
 import Footer from './components/footer/footer';
 
-import './App.css';
+// import './App.css';
 function App() {
   return (
     <div className="App">
-        <Header />
+        {/* <Header /> */}
         <Main />
-        <Footer />
+        {/* <Footer /> */}
     </div>
   );
 }
--- my-app/src/components/main/main.js
+++ my-app/src/components/main/main.js
@@ -1,11 +1,11 @@
-import logo from '../../renek.jpg';
-import '../../App.css'
+import logo from '../../homem-feliz-dinheiro.jpg';
+// import '../../App.css'
 import './main.css'
 const Main = () => {
     return (
         <main>
-            <img src={logo} alt="logo" class="App-logo" />
             <h1>Este é meu primeiro site com react</h1>
+            <img src={logo} alt="logo" class="App-logo" />
             <h2>Hello World!</h2>
         </main>
     )
