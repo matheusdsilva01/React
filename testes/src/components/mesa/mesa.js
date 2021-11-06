41ed8f63281e9857e1d6f1dde736c7bbecd20331
Matheus da Silva <ms25022003@gmail.com>
2021-11-06 10:38:38 -0300

    Funcional

--- testes/src/components/mesa/mesa.js
+++ testes/src/components/mesa/mesa.js
@@ -5,7 +5,6 @@ export default class mesa extends Component {
         super();
         this.state = {
             valor: 0,
-            mostrarNome: false,
             lista: [{
                 id: 1,
                 nome: 'Arroz',
@@ -19,34 +18,47 @@ export default class mesa extends Component {
         };
     }
     render() {
-        /* const mostrarNome = () => {
-            this.setState({ mostrarNome: !this.state.mostrarNome })
-        } */
         const removerNome = (idItem) => {
-            const lista = this.state.lista.filter(({id}) => id !== idItem);
+            console.log(this.state.lista);
+            const lista = this.state.lista.filter(({ id }) => id !== idItem);
             this.setState({ lista: lista });
         }
-        
+
+        const mostrarLista = () => {
+            this.setState(
+                {
+                    lista: [{
+                        id: 1,
+                        nome: 'Arroz',
+                        imagem: 'https://vivareceita-cdn.s3.amazonaws.com/uploads/2020/11/Aprenda-como-fazer-um-arroz-dos-deuses.-Fonte-Pinterest.jpg',
+                        mostrar: true
+                    },
+                    {
+                        id: 2,
+                        nome: 'Bifé',
+                        imagem: 'https://swiftbr.vteximg.com.br/arquivos/ids/178617-768-768/bife-alcatra-swift-900g-616163-1.jpg?v=637704160149470000',
+                        mostrar: true
+                    }]
+                }
+            )
+            }
 
         return (
             <>
-            
                 <h1>Mesa de trabalho</h1>
-
-            <div className="d-flex flex-wrap flex-column justify-content-center align-items-center py-3 my-4">
-                {
-                    this.state.lista.map(({ id, nome, imagem}) => {
+                    <button onClick={() => mostrarLista()}>
+                        Voltar
+                    </button>
+                <div>
+                    {this.state.lista.map(({ id, nome, imagem }) => {
                         return (
                             <div key={id} onClick={() => removerNome(id)}>
                                 <h3>{nome}</h3>
                                 <img src={imagem} alt={nome} />
                             </div>
                         )
-                    })
-                }
-            </div>
-
-
+                    })}
+                </div> 
             </>
         )
     }
